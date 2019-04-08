73e6b85b2594414d5df560310908c2b56c6c8574
superdba111 <yingyou@gmail.com>
2019-04-08 14:24:00 -0500

    Updates

---
+++ precache-manifest.a60b2ca8a2e5acdc0f583f0edf995d5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d0617411f253188ef25",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "0d0617411f253188ef25",
+    "url": "/contactmanager/static/js/main.3803eb47.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "8375415d4ecbdd42903c",
+    "url": "/contactmanager/static/css/2.c5191b78.chunk.css"
+  },
+  {
+    "revision": "8375415d4ecbdd42903c",
+    "url": "/contactmanager/static/js/2.a46c82bc.chunk.js"
+  },
+  {
+    "revision": "b6b28994df237eb5120acab526031497",
+    "url": "/contactmanager/index.html"
+  }
+];
